5ea2a88aa2894914c00a2227d61fa9f54756a1e9
Benoît Perez-Lamarque <benoit.perez.lamarque@gmail.com>
2022-01-06 12:39:19 +0100

    add scripts

---
+++ Metabarcoding/pipeline_OTU_Swarm.sh
@@ -0,0 +1,448 @@
+#!/bin/bash
+
+#####################################################################################################
+######################  PERFORM OTU CLUSTERING FROM  METABARCODING DATASETS USING SWARM #############
+#####################################################################################################
+
+
+## You first need to install PYTHON3, VSEARCH, CUTADAPT and SWARM (v3)
+## see https://github.com/frederic-mahe/swarm/wiki/Fred's-metabarcoding-pipeline for details
+
+
+# You also need to download the python scripts OTU_contingency_table.py, map2qiime.py, and make_stats.py available in https://github.com/BPerezLamarque/Scripts/blob/master/Metabarcoding/
+# Store them in a given folder
+path_scripts="PATH_TOWARD_THE_SCRIPTS"
+
+
+
+#####################################################################################################
+######################  STEP0: PREPARE THE DATABASE FOR THE TAXONOMIC ASSIGNATION     ###############
+#####################################################################################################
+
+
+
+##### If you are working with the ITS2 region (e.g. with the primers ITS86F and ITS4)   ##############
+
+# Download the UNITE database (https://unite.ut.ee)
+# use 97% OTU of  Fungi + other Eukaryotes
+
+
+path="YOUR_WORKING_DIRECTORY"
+cd $path
+
+mkdir database/
+cd $path"/database/"
+
+
+INPUT="UNITE_all_Euk_OTU97_1DB95C8AC0A80108BECAF1162D761A8D379AF43E2A4295A3EF353DD1632B645B.fasta" # UNITE database that you have previoulsy mannualt downloaded
+
+
+# Define variables and output files for your primers
+OUTPUT="UNITE_all_Euk_OTU97_ITS2_86F.fasta"
+LOG="UNITE_all_Euk_OTU97_ITS2_86F.log"
+
+
+# Precise the sequences of the primers you used for the metabarcoding
+PRIMER_F="GTGAATCATCGAATCTTTGAA" # ITS86f
+# Do not check for ITS4 because it is outside of most of the ITS sequences in the UNITE  database
+MIN_LENGTH=200
+MIN_F=$(( ${#PRIMER_F} / 2 ))
+
+CUTADAPT="cutadapt --discard-untrimmed --minimum-length ${MIN_LENGTH}"
+
+# Trim forward & reverse primers, format
+cat "${INPUT}" | sed '/^>/ ! s/U/T/g' | \
+${CUTADAPT} -g "${PRIMER_F}" -O "${MIN_F}" - 2> "${LOG}" | \
+sed '/^>/ s/;/|/g ; /^>/ s/ /_/g ; /^>/ s/_/ /1' > "${OUTPUT}"
+
+
+
+
+##### If you are working with the 16S or 18S region (e.g. with the primers 799F and 1193R of the 16S region)   ##############
+
+
+path="YOUR_WORKING_DIRECTORY"
+cd $path
+
+mkdir database/
+cd $path"/database/"
+
+
+RELEASE=138.1
+URL="https://ftp.arb-silva.de/release_${RELEASE}/Exports"
+INPUT="SILVA_${RELEASE}_SSURef_NR99_tax_silva.fasta.gz"
+
+# Download and check
+wget -c ${URL}/${INPUT}{,.md5} && md5 -r ${INPUT}.md5
+
+# Define variables and output files for your primers
+OUTPUT="${INPUT/.fasta.gz/_799F_1193R.fasta}"
+LOG="${INPUT/.fasta.gz/_799F_1193R.log}"
+
+
+# Precise the sequences of the primers you used for the metabarcoding
+PRIMER_F="AACMGGATTAGATACCCKG"
+PRIMER_R="ACGTCATCCCCACCTTCC"
+ANTI_PRIMER_R="GGAAGGTGGGGATGACGT"
+MIN_LENGTH=32
+MIN_F=$(( ${#PRIMER_F} / 2 ))
+MIN_R=$(( ${#PRIMER_R} / 2 ))
+CUTADAPT="cutadapt --discard-untrimmed --minimum-length ${MIN_LENGTH}"
+
+# Trim forward & reverse primers, format
+zcat < "${INPUT}" | sed '/^>/ ! s/U/T/g' | \
+${CUTADAPT} -g "${PRIMER_F}" -O "${MIN_F}" - 2> "${LOG}" | \
+${CUTADAPT} -a "${ANTI_PRIMER_R}" -O "${MIN_R}" - 2>> "${LOG}" | \
+sed '/^>/ s/;/|/g ; /^>/ s/ /_/g ; /^>/ s/_/ /1' > "${OUTPUT}"
+
+rm ${INPUT}
+
+
+
+
+#####################################################################################################
+######################     STEP1: MERGE R1 & R2 paired-ended sequences	       ######################
+#####################################################################################################
+
+
+# Step 1-A: Prepare the data
+
+
+# Give the name of your marker gene
+Marker="ITS2"
+# or Marker="16S"
+
+# Download the data and store them in a folder "raw_data"
+
+path="YOUR_WORKING_DIRECTORY"
+cd $path
+
+mkdir raw_data/
+cd $path"/raw_data/"
+
+# Write the name of the paired-ended sequences
+INPUT_R1="../raw_data/210920_SN234_A_L001_ATXJ-7_R1.fastq"  # bz2, gz and uncompressed fastq files are allowed
+INPUT_R2="../raw_data/210920_SN234_A_L001_ATXJ-7_R2.fastq"
+
+
+OUTPUT="Quality.encoding_"$Marker".log"
+
+# Check the quality encoding (33 or 64?)
+vsearch \
+    --fastq_chars ${INPUT_R1} 2> ${OUTPUT/.fastq/.log}
+
+
+# Step 1-B: Merge the paired-end reads
+
+THREADS=2
+ENCODING=33
+OUTPUT="MERGED_R1R2_"$Marker".fastq"
+OUTPUT_NOTMERGED="UNMERGED_R1R2_"$Marker
+
+
+vsearch \
+    --threads ${THREADS} \
+    --fastq_mergepairs ${INPUT_R1} \
+    --reverse ${INPUT_R2} \
+    --fastq_ascii ${ENCODING} \
+    --fastqout ${OUTPUT} \
+    --fastq_allowmergestagger \
+    --quiet 2>> ${OUTPUT/.fastq/.log}
+    
+    
+# Step 1-C: Checking the quality with FASTQC
+
+# FastQC is available on https://www.bioinformatics.babraham.ac.uk/projects/download.html#fastqc (https://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.7.zip)
+mkdir FastQC/
+/PATH_TO_FastQC/fastqc -t 4 ${OUTPUT} -o FastQC/
+
+
+
+
+#####################################################################################################
+######################  			STEP2: DEMULTIPLEX SEQUENCES	           ######################
+#####################################################################################################
+
+
+# Your 'mapping' file should contain at least 5 columns, the first one being the sample names (starting with '#'), and the following being, not necessary in this order but with the...
+# ... exact names : "primerFw", "primerRev", "barcodeFw", "barcodeRev".
+# see example in https://github.com/BPerezLamarque/Scripts/blob/master/Metabarcoding/mapping_example_ITS.txt
+
+
+# Step 2-A: Check the  mapping file
+
+
+# Give the name of your marker gene
+Marker="ITS2"
+# or Marker="16S"
+
+MAPPING="mapping_"$Marker".txt"
+
+
+barcodeFwColumnIdx="$(( $(sed -n $'1s/\t/\\\n/gp' $MAPPING | grep -nx 'barcodeFw' | cut -d: -f1) - 1 ))" # getting the index of column with forward barcode etc. "-1" is applied as array below counts from 0
+primerFwColumnIdx="$(( $(sed -n $'1s/\t/\\\n/gp' $MAPPING | grep -nx 'primerFw' | cut -d: -f1) -1 ))"
+barcodeRevColumnsIdx="$(( $(sed -n $'1s/\t/\\\n/gp' $MAPPING | grep -nx 'barcodeRev' | cut -d: -f1) -1 ))"
+primerRevColumnsIdx="$(( $(sed -n $'1s/\t/\\\n/gp' $MAPPING | grep -nx 'primerRev' | cut -d: -f1) -1 ))"
+
+
+# Check that all the samples are present
+while read -r line; do
+    if [[ ! "$line" =~ ^#.* && ! "$line" =~ ^Sample.* ]]; then
+        IFS=$'\t' read -r -a array <<< "$line"
+        
+        # Get sequences
+        FwBarcode="${array[${barcodeFwColumnIdx}]}"
+        FwPrimer="${array[${primerFwColumnIdx}]}"
+        RevBarcode="${array[${barcodeRevColumnsIdx}]}"
+        RevBarcodeRC=$( echo "${RevBarcode}" | tr ACGTacgtYyMmRrKkBbVvDdHh TGCAtgcaRrKkYyMmVvBbHhDd | rev )
+        RevPrimer="${array[${primerRevColumnsIdx}]}"
+        RevPrimerRC=$( echo "${RevPrimer}" | tr ACGTacgtYyMmRrKkBbVvDdHh TGCAtgcaRrKkYyMmVvBbHhDd | rev )
+        SAMPLE_NAME="${array[0]}"
+        
+        # Some information
+        echo "${SAMPLE_NAME} is being tested.."
+        echo "Barcode Fw: ${FwBarcode}"
+        echo "Primer Fw: ${FwPrimer}"
+        echo "Primer Rev (RC): ${RevPrimerRC}"
+        echo "Barcode Rev (RC): ${RevBarcodeRC}"
+    fi
+    
+done < "${MAPPING}"
+
+
+# Step 2-B: Perform the quality filtering
+
+cd $path
+
+# Discard sequences containing Ns, remove if >1 error on average, convert to fasta
+vsearch \
+    --fastq_filter "MERGED_R1R2_"$Marker".fastq" \
+    --fastq_maxns 0 \
+    --fastq_maxee 2 \
+    --fastaout "MERGED_R1R2_"$Marker".fasta"
+    
+
+ 
+# Step 2-C: Demultiplex the sequences
+ 
+ 
+# Define binaries, temporary files and output files
+INPUT="MERGED_R1R2_"$Marker".fasta"
+INPUT_REVCOMP="${INPUT/.fasta/_RC.fasta}"
+
+
+# Reverse complement fastq file
+vsearch --quiet \
+    --fastx_revcomp "${INPUT}" \
+    --fastaout "${INPUT_REVCOMP}"
+
+
+mkdir $path/"Demultiplexed_data_"$Marker/
+
+MIN_LENGTH=200
+
+
+while read -r line; do
+    if [[ ! "$line" =~ ^#.* && ! "$line" =~ ^Sample.* ]]; then
+        IFS=$'\t' read -r -a array <<< "$line"
+
+        # Get sequences
+        FwBarcode="${array[${barcodeFwColumnIdx}]}"
+        FwPrimer="${array[${primerFwColumnIdx}]}"
+        RevBarcode="${array[${barcodeRevColumnsIdx}]}"
+        RevBarcodeRC=$( echo "${RevBarcode}" | tr ACGTacgtYyMmRrKkBbVvDdHh TGCAtgcaRrKkYyMmVvBbHhDd | rev )
+        RevPrimer="${array[${primerRevColumnsIdx}]}"
+        RevPrimerRC=$( echo "${RevPrimer}" | tr ACGTacgtYyMmRrKkBbVvDdHh TGCAtgcaRrKkYyMmVvBbHhDd | rev )
+        SAMPLE_NAME="${array[0]}"
+        
+        # Output file names
+        LOG="Demultiplexed_data_"$Marker"/${SAMPLE_NAME}.log"
+        FINAL_FASTA="Demultiplexed_data_"$Marker"/${SAMPLE_NAME}.fas"
+        
+        # Some information
+        echo "${SAMPLE_NAME} is being processed.."
+        echo "Barcode Fw: ${FwBarcode}"
+        echo "Primer Fw: ${FwPrimer}"
+        echo "Primer Rev (RC): ${RevPrimerRC}"
+        echo "Barcode Rev (RC): ${RevBarcodeRC}"
+        
+        function trim_without_ambiguity {
+
+            SEQTOT="${FwBarcode}${FwPrimer}${RevPrimerRC}${RevBarcodeRC}"
+            MIN_MATCHED=${#SEQTOT}
+            ERROR_RATE=0
+
+            cat "${INPUT}" "${INPUT_REVCOMP}" | cutadapt -g "${FwBarcode}${FwPrimer}...${RevPrimerRC}${RevBarcodeRC}" --discard-untrimmed --minimum-length "${MIN_LENGTH}" -O ${MIN_MATCHED} -e "${ERROR_RATE}" - 2> "${LOG}" > "temp_"$Marker".fasta"
+        }
+        
+
+        trim_without_ambiguity
+
+        # Dereplicate at the study level
+        vsearch --quiet \
+            --derep_fulllength "temp_"$Marker".fasta" \
+            --sizein \
+            --sizeout \
+            --fasta_width 0 \
+            --relabel_sha1 \
+            --output "${FINAL_FASTA}" 2>> "${LOG}"
+
+    fi
+done < "${MAPPING}"
+
+
+# Note: the option sha1 (encoding system) is giving the same names to the identical amplicons across samples
+
+
+# Remove the files that are no longer useful
+rm -f "${INPUT}" "${INPUT_REVCOMP}" "temp_"$Marker".fastq" "temp_"$Marker".fasta"
+
+
+
+
+###############################################################################################
+###############################   STEP3:  SWARM CLUSTERING         ############################
+###############################################################################################
+
+
+## to see all the details: https://github.com/frederic-mahe/swarm/wiki/Fred's-metabarcoding-pipeline
+
+path="YOUR_WORKING_DIRECTORY"
+cd $path
+
+mkdir process/
+cd $path"/process/"
+
+Marker="ITS2"
+database_taxonomy="UNITE_all_Euk_OTU97_ITS2_86F.fasta"
+
+# or : Marker="16S
+# database_taxonomy="SILVA_138.1_SSURef_NR99_tax_silva_799F_1193R.fasta"
+
+
+
+# Step 3-A: Dereplicate of fasta file of every  individual sample  using VSEARCH (already done in Step 2)
+
+# Important: for every sample, you must have a separate file containing all the metabarcoding reads from this sample that are **dereplicated with the option relabel_sha1** (as done in Step 2).
+
+
+
+# Step 3-B: Dereplication of fasta file of all the reads using VSEARCH
+
+PREFIX=""
+cat $path/"Demultiplexed_data_"$Marker/"$PREFIX"*.fas > "reads_"$Marker".fa"
+
+
+vsearch \
+    --derep_fulllength "reads_"$Marker".fa" \
+    --sizein \
+    --sizeout \
+    --relabel_sha1 \
+    --fasta_width 0 \
+    --output "reads_"$Marker"_derep.fa"
+
+
+rm "reads_"$Marker".fa"
+
+
+# Total number of reads in the dataset
+grep ">" "reads_"$Marker"_derep.fa" | sed 's/>.*.;size=//g' | paste -sd+ - | bc
+
+
+
+##  Step 3-C: SWARM clustering
+
+swarm=/PATH_TOWARD_SWARM/swarm-3.0.0-macos-x86_64/bin/swarm
+
+
+THREADS=2  #number of cores
+
+$swarm \
+    -d 1 -f -t ${THREADS} --bloom-bits 4 -z \
+    -i "swarm_"$Marker"_otu_1f.struct" \
+    -s "stats_"$Marker"_swarm_1f.txt" \
+    -w "reads_"$Marker"_swarm_1f.fa" \
+    -o "reads_"$Marker"_mapped_swarm_1f.txt" < "reads_"$Marker"_derep.fa"
+
+
+# Sort representatives
+vsearch --fasta_width 0 \
+    --sortbysize "reads_"$Marker"_swarm_1f.fa" \
+    --output "reads_"$Marker"_swarm_1f_final.fa"
+
+
+
+##  Step 2-D: Chimera checking
+
+vsearch --uchime_denovo "reads_"$Marker"_swarm_1f_final.fa" \
+    --uchimeout "reads_"$Marker"_swarm_1f.uchime" --nonchimeras "reads_"$Marker"_swarm_1f_nonchimeras.fa"
+
+
+
+
+##  Step 2-E: Assign taxonomy to OTUs using known reference sequences with blast
+
+
+# Number of threads can be set to 1
+vsearch --usearch_global "reads_"$Marker"_swarm_1f_nonchimeras.fa" \
+--threads 2 \
+--dbmask none \
+--qmask none \
+--rowlen 0 \
+--notrunclabels \
+--userfields query+id1+target \
+--maxaccepts 0 \
+--maxrejects 32 \
+--top_hits_only \
+--output_no_hits \
+--db "../database/"$database_taxonomy \
+--id 0.5 \
+--iddef 4 \
+--userout "taxonomy_otu_"$Marker"_swarm_1f.txt"
+
+
+
+
+##  Step 2-F:  Make the OTU table
+
+STATS="stats_"$Marker"_swarm_1f.txt"
+OTUS="reads_"$Marker"_mapped_swarm_1f.txt"
+REPRESENTATIVES="reads_"$Marker"_swarm_1f_final.fa"
+UCHIME="reads_"$Marker"_swarm_1f.uchime"
+ASSIGNMENTS="taxonomy_otu_"$Marker"_swarm_1f.txt"
+OTU_TABLE="OTU_table_"$Marker"_swarm_1f.txt"
+
+
+SCRIPT=$path_scripts"/OTU_contingency_table.py"  # this script is located https://github.com/BPerezLamarque/Scripts/blob/master/Metabarcoding/OTU_contingency_table.py
+
+python3 \
+    "${SCRIPT}" \
+    "${REPRESENTATIVES}" \
+    "${STATS}" \
+    "${OTUS}" \
+    "${UCHIME}" \
+    "${ASSIGNMENTS}" \
+    $path/"Demultiplexed_data_"$Marker/"$PREFIX"*.fas > "${OTU_TABLE}"
+
+
+# Filter per OTU size or spread, quality and chimeric status:
+TABLE="OTU_table_"$Marker"_swarm_1f.txt"
+FILTERED="${TABLE/.txt/_filtered.txt}"
+
+head -n 1 "${TABLE}" > "${FILTERED}"
+cat "${TABLE}" | awk '$5 == "N" && $4 >= 200 && $2 >= 10 && $6 >= 1' >> "${FILTERED}"
+# remove chimera, keep OTU of more than 200 bp, with an abundance of at least 10 reads, and spread of >=1
+
+
+# Total number of reads in the OTU table
+grep -v "OTU\tabundance" "${TABLE}" | cut -f2 | paste -sd+ - | bc
+
+# Total number of reads in the filtered OTU table
+grep -v "OTU\tabundance" "${FILTERED}" | cut -f2 | paste -sd+ - | bc
+
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 # Scripts
 
 
-The folder  `Ariamnes_microbiota`  contains the functions to reproduce the microbiota analyses from the manuscript: *A holobiont view of island biogeography: unraveling factors driving the nascent diversification of a Hawaiian spider and its microbial associates*, by Ellie E. Armstrong, Benoît Perez-Lamarque, Ke Bi, Cerise Chen, Leontine E. Becking, Benjamin Peter, Jun Ying Lim, Tyler Linderoth, Rosemary Gillespie, Stefan Prost, Henrik Krehenwinkel (*in prep.*).
+## Pipelines for dealing with metabarcoding data
+
+The folder  `Metabarcoding`  contains bash scripts to analyses metabarcoding data from Illumina paired-end sequencing. They can either be used to perform [OTUs at (or any other thresholds)](https://github.com/BPerezLamarque/Scripts/blob/master/Metabarcoding/pipeline_OTU97%.sh), [ASV](https://github.com/BPerezLamarque/Scripts/blob/master/Metabarcoding/pipeline_ASV.sh), or  [Swarm OTUs](https://github.com/BPerezLamarque/Scripts/blob/master/Metabarcoding/pipeline_OTU_Swarm.sh) ([Mahé et al., 2014](https://peerj.com/articles/593/); [Mahé et al., 2015](https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4690345/pdf/peerj-03-1420.pdf)). 
+
+These scripts are derieved from [Frédéric Mahé's metabarcoding pipeline](https://github.com/frederic-mahe/swarm/wiki/Fred's-metabarcoding-pipeline) and are mainly using [VSEARCH](https://github.com/torognes/vsearch). They are composed of 4 steps: the Step 0 prepares a database for the taxonomic assignation, Step 1 merges the paired-end reads, Step 2 demultiplexes the sequences (i.e. assignes each read to its sample), and Step 3 performs the clustering. 
+
+**Contact:** Benoît Perez-Lamarque, benoit.perez.lamarque@gmail.com
+
+
+
+
+## Scripts for *Ariamnes* microbiota analyses 
+
+The folder  `Ariamnes_microbiota`  contains the functions to reproduce the microbiota analyses from the manuscript: *A holobiont view of island biogeography: Unravelling patterns driving the nascent diversification of a Hawaiian spider and its microbial associates*, by Ellie E. Armstrong, Benoît Perez-Lamarque, Ke Bi, Cerise Chen, Leontine E. Becking, Jun Ying Lim, Tyler Linderoth, Henrik Krehenwinkel, Rosemary G. Gillespie, 2021, in Molecular Ecology, https://doi.org/10.1111/mec.16301.
 
 
 **Contact:** Benoît Perez-Lamarque, benoit.perez.lamarque@gmail.com
